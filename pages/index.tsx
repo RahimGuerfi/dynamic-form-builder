4b2a26424ad14eb1fc9b473bdcca2f68fb15e719
RahimGuerfi <89612748+RahimGuerfi@users.noreply.github.com>
2022-08-14 14:32:59 +0100

    testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
 import type { NextPage } from "next";
+import InputCreator from "../components/InputCreator";
+import FormLayout from "../components/FormLayout";
 
 const Home: NextPage = () => {
-  return <h1 className="text-3xl text-center text-green-700">Hello World</h1>;
+  return (
+    <>
+      <h1 className="text-3xl text-center text-green-700">Hello World</h1>
+      <InputCreator />
+      <hr />
+      <FormLayout />
+    </>
+  );
 };
 
 export default Home;
