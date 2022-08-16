744c94f2733fc2be6bab5f6d8450efea34cf4a2f
RahimGuerfi <89612748+RahimGuerfi@users.noreply.github.com>
2022-08-16 01:14:21 +0100

    Updated FormLayout and InputCreator

--- components/FormLayout/Item.tsx
+++ components/FormLayout/Item.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { useStore } from "../../lib/store";
 import { FormComponent } from "../../types";
+import { Reorder } from "framer-motion";
+import { TbEditOff, TbEdit, TbTrash, TbGripVertical } from "react-icons/tb";
+import { TextMd } from "../Atoms";
 
 interface Props {
   data: FormComponent;
@@ -9,28 +12,51 @@ interface Props {
 const Item = ({ data }: Props) => {
   const { removeComponent, componentToEdit, setComponentToEdit } = useStore();
 
+  //Return if current component is being edited
+  const isEditing = () => componentToEdit?.id === data.id;
+
   //Remove component from store
   const handleDelete = () => {
     if (confirm("Are you sure you want to delete this?")) {
+      if (isEditing()) setComponentToEdit(null);
       removeComponent(data.id);
-      if (componentToEdit?.id === data.id) setComponentToEdit(null);
     }
   };
 
   //Set component to edit
   const toggleEdit = () => {
-    if (componentToEdit?.id === data.id) setComponentToEdit(null);
+    if (isEditing()) setComponentToEdit(null);
     else setComponentToEdit(data);
   };
 
   return (
-    <div>
-      <p>{data.inputName}</p>
-      <button onClick={toggleEdit}>
-        {componentToEdit?.id === data.id ? "CANCEL EDIT" : "EDIT"}
-      </button>
-      <button onClick={handleDelete}>Delete</button>
-    </div>
+    <Reorder.Item
+      whileDrag={{ opacity: 0.85, transition: { duration: 0.15 } }}
+      className={`p-3 mb-3 transition-[background] duration-500 rounded-lg flex justify-between items-center cursor-grab shadow ${
+        isEditing() ? "bg-gray-200" : "bg-gray-100"
+      }`}
+      value={data}
+    >
+      <div className="flex items-center justify-center gap-2">
+        <TbGripVertical className="text-teal-600" size="24px" />
+        <TextMd text={data.inputName} />
+      </div>
+
+      <div className="flex gap-2">
+        <button
+          onClick={toggleEdit}
+          className="p-1 focus:outline-none focus:shadow-outline text-teal-500 hover:text-teal-600"
+        >
+          {isEditing() ? <TbEditOff size="24px" /> : <TbEdit size="24px" />}
+        </button>
+        <button
+          onClick={handleDelete}
+          className="p-1 focus:outline-none focus:shadow-outline text-red-600 hover:text-red-800"
+        >
+          <TbTrash size="24px" />
+        </button>
+      </div>
+    </Reorder.Item>
   );
 };
 
